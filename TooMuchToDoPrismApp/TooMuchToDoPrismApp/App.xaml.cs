2180bb91c2a9423f71c3de51e70a57cff9c8cea0
Katherine Hambley <superdesigngirl@mac.com>
2021-11-04 14:12:48 -0500

    Add project files.

---
+++ TooMuchToDoPrismApp/TooMuchToDoPrismApp/App.xaml.cs
@@ -0,0 +1,33 @@
+using Prism;
+using Prism.Ioc;
+using TooMuchToDoPrismApp.ViewModels;
+using TooMuchToDoPrismApp.Views;
+using Xamarin.Essentials.Implementation;
+using Xamarin.Essentials.Interfaces;
+using Xamarin.Forms;
+
+namespace TooMuchToDoPrismApp
+{
+	public partial class App
+	{
+		public App(IPlatformInitializer initializer)
+			: base(initializer)
+		{
+		}
+
+		protected override async void OnInitialized()
+		{
+			InitializeComponent();
+
+			await NavigationService.NavigateAsync("NavigationPage/MainPage");
+		}
+
+		protected override void RegisterTypes(IContainerRegistry containerRegistry)
+		{
+			containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+
+			containerRegistry.RegisterForNavigation<NavigationPage>();
+			containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+		}
+	}
+}
---
+++ TooMuchToDoPrismApp/TooMuchToDoPrismApp/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TooMuchToDoPrismApp.Views
+{
+	public partial class MainPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
