e47e00b92a8b2a3e1b2f1edddfe2877a15245774
Katherine Hambley <superdesigngirl@mac.com>
2021-11-05 15:22:30 -0500

    Added repository and sqlite

---
+++ TooMuchToDoPrismApp/TooMuchToDoPrismApp/Repositories/TodoItemRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TooMuchToDoPrismApp.Models;
+using System.Threading.Tasks;
+
+namespace TooMuchToDoPrismApp.Repositories
+{
+	public class TodoItemRepository : ITodoItemRepository
+	{
+		// used to notify subscribers of a list that items have been added or updated.
+		public event EventHandler<TodoItem> OnItemAdded;
+		public event EventHandler<TodoItem> OnItemUpdated;
+
+		public async Task<List<TodoItem>> GetItems()
+		{
+			return null; // Just to get it to build for now.
+		}
+
+		public async Task AddItem(TodoItem item)
+		{
+
+		}
+		public async Task UpdateItem(TodoItem item)
+		{
+
+		}
+
+		public async Task AddOrUpdate(TodoItem item)
+		{
+			if(item.Id == 0)
+			{
+				await AddItem(item);
+			}
+			else
+			{
+				await UpdateItem(item);
+			}
+		}
+
+	}
+}
