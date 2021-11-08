2894c3951e40b4476d8fd75fdb064a883b21b777
Katherine Hambley <superdesigngirl@mac.com>
2021-11-08 07:33:10 -0600

    Added StatusColorConverter to MainPage for completed tasks

---
+++ TooMuchToDoPrismApp/TooMuchToDoPrismApp/Converters/StatusColorConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TooMuchToDoPrismApp.Converters
+{
+	public class StatusColorConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			//these values are accessed in the Resources dictionary in App.xaml file
+			return (bool)value ? (Color)Application.Current.Resources["CompletedColor"] : (Color)Application.Current.Resources["ActiveColor"];
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+	}
+}
